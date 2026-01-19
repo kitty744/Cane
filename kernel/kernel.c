b5981c61481ac70636a9fef6bd65902fdefefc55
Trollycat <kittygamingbuisness@gmail.com>
2026-01-19 08:47:00 -0500

    Needed update to memory.

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -15,23 +15,23 @@ void kmain(unsigned long magic, unsigned long addr)
 {
     clear_screen();
  
+
     if (magic != MULTIBOOT2_BOOTLOADER_MAGIC)
     {
-        printf("Invalid multiboot magic: 0x%lx\n", magic);
         while (1)
-            asm volatile("hlt");
+            ;
     }
- 
+
     uint64_t max_physical_addr = 0;
     struct multiboot_tag_mmap *mmap_tag = NULL;
- 
+
     struct multiboot_tag *tag = (struct multiboot_tag *)PHYS_TO_VIRT(addr + 8);
     while (tag->type != MULTIBOOT_TAG_TYPE_END)
     {
         if (tag->type == MULTIBOOT_TAG_TYPE_MMAP)
         {
             mmap_tag = (struct multiboot_tag_mmap *)tag;
-            uint32_t entries = (mmap_tag->size - 8) / 24;
+            uint32_t entries = (mmap_tag->size - sizeof(struct multiboot_tag_mmap)) / mmap_tag->entry_size;
             for (uint32_t i = 0; i < entries; i++)
             {
                 if (mmap_tag->entries[i].type == MULTIBOOT_MEMORY_AVAILABLE)
@@ -44,20 +44,20 @@ void kmain(unsigned long magic, unsigned long addr)
         }
         tag = (struct multiboot_tag *)((uint8_t *)tag + ((tag->size + 7) & ~7));
     }
- 
+
     if (max_physical_addr == 0)
         max_physical_addr = 0x20000000;
- 
+
     uintptr_t kernel_phys_end = VIRT_TO_PHYS((uintptr_t)_kernel_end);
     uintptr_t bitmap_phys = (kernel_phys_end + 0x1000) & ~0xFFFULL;
-    pmm_init(bitmap_phys, max_physical_addr);
- 
+    pmm_init((uintptr_t)PHYS_TO_VIRT(bitmap_phys), max_physical_addr);
+
     if (mmap_tag)
     {
-        uint32_t entries = (mmap_tag->size - 8) / 24;
+        uint32_t entries = (mmap_tag->size - sizeof(struct multiboot_tag_mmap)) / mmap_tag->entry_size;
         uint64_t bitmap_size = (max_physical_addr / 32768) + 4096;
         uintptr_t b_end = bitmap_phys + bitmap_size;
- 
+
         for (uint32_t i = 0; i < entries; i++)
         {
             if (mmap_tag->entries[i].type == MULTIBOOT_MEMORY_AVAILABLE)
